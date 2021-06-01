310d4b775ed1102960de067f6a87c56f64073739
Muneeb <muneebwanee@gmail.com>
2021-06-01 12:58:25 +0530

    Update password.js

--- JavaScripts/password.js
+++ JavaScripts/password.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
   //Password Feature by DeepNet
- function verify(){if (document.querySelector('#password1').value === 'd-eepnet')(document.querySelector('#password2').value === 'de-epnet')(document.querySelector('#password3').value === 'dee-pnet'){document.querySelector('.deepnetloginwrap').classList.add('hidden')}
+ function verify(){if (document.querySelector('#password').value === 'ok'){document.querySelector('.deepnetloginwrap').classList.add('hidden')}
 else{alert('Password Yang Anda Masukan Salah!');password.setSelectionRange(0,password.value.length)}
 return false}
   const show = () => {
